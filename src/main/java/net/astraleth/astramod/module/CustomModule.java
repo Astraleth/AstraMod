75f0bca000df6d5aeefd4b33f7126723ae72f37f
Amraleth <amraleth@proton.me>
2024-07-11 22:53:08 +0200

    Add base module classes

---
+++ src/main/java/net/astraleth/astramod/module/CustomModule.java
@@ -0,0 +1,18 @@
+package net.astraleth.astramod.module;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * The base class used for every module
+ *
+ * @param <T> The main classed used for initializing the module
+ */
+public interface CustomModule<T extends ModuleBase> {
+
+    /**
+     * The first method called when initializing a module
+     *
+     * @param baseClass The base class used for initializing
+     */
+    void initModule(@NotNull T baseClass);
+}
---
+++ src/main/java/net/astraleth/astramod/module/ModuleBase.java
@@ -0,0 +1,7 @@
+package net.astraleth.astramod.module;
+
+/**
+ * Used for identifying the base class of the project
+ */
+public abstract class ModuleBase {
+}
